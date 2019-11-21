a1c040dd701d4289b83d9614bcfad401c21e2c61
Jonathan Morford <git@jonathan.morford.xyz>
2019-11-21 15:15:51 -0500

    Lint formatting fixes

--- src/main/app.js
+++ src/main/app.js
@@ -3,17 +3,17 @@ var app = express()
 
 // The code below will display 'Hello World!' to the browser when you go to http://localhost:3000
 app.get('/', (req, res) => {
-    // the .status(200) isn't necessary since it is the default return status
-    res.status(200).send('Hello World!')
+  // the .status(200) isn't necessary since it is the default return status
+  res.status(200).send('Hello World!')
 })
 
 app.get('/inverted', (req, res) => {
-    // the .status(200) isn't necessary since it is the default return status
-    res.status(200).send('!dlroW olleH')
+  // the .status(200) isn't necessary since it is the default return status
+  res.status(200).send('!dlroW olleH')
 })
 
 app.listen(3000, () => {
-    console.log('Example app listening on http://localhost:3000')
+  console.log('Example app listening on http://localhost:3000')
 })
 
-module.exports = app;
+module.exports = app
